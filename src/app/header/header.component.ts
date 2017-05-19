9c3e95d52947d4601602b73476471e769485f514
Daniel Straus <strausd@me.com>
2017-05-19 15:12:28 -0500

    Setup basic navigation routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Output, EventEmitter} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,9 +6,5 @@ import {Component, Output, EventEmitter} from '@angular/core';
     styleUrls: ['header.component.css']
 })
 export class HeaderComponent {
-  @Output() showRecipes = new EventEmitter<string>();
 
-  onSelect(feature: string) {
-    this.showRecipes.emit(feature);
-  }
 }
