92b2a74eaddf4b089765855038885897c48f02dc
Daniel Straus <strausd@me.com>
2017-05-30 16:05:34 -0500

    Cleanup shopping-list and auth modules

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -5,7 +5,7 @@ import {RecipesComponent} from './recipes.component';
 import {RecipeStartComponent} from './recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
 import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
-import { AuthGuard } from '../auth/auth-guard.service';
+import {AuthGuard} from '../auth/auth-guard.service';
 
 
 const recipesRoutes: Routes = [
